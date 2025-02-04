ae868ded334882d1cd65a9b19d6cc2c480399eb9
Joseph Phillips <joe.phillips@canonical.com>
2025-02-04 08:47:27 +0100

    feat: adds force column to removal table
    The original notion was to imply forced removals with the job type as
    was done when backed with MongoDB, but it is stronger architecture to
    enshrine it in the schema. Realistically, we won't ever be doing away
    with the forced removal concept.

--- domain/schema/model/sql/0026-cleanup.sql
+++ domain/schema/model/sql/0026-cleanup.sql
@@ -12,7 +12,10 @@ INSERT INTO removal_type VALUES
 CREATE TABLE removal (
     uuid TEXT NOT NULL PRIMARY KEY,
     removal_type_id INT NOT NULL,
+    force BOOLEAN NOT NULL DEFAULT false,
     entity_uuid TEXT NOT NULL,
+    -- This is the earliest time that the job will be actioned if picked up by
+    -- the watcher. It allows us to schedule a removal job in the future.
     scheduled_for DATETIME NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'utc')),
     arg TEXT,
     CONSTRAINT fk_removal_type
