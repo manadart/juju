01e83ae7321c1bc9bd92203310f032ccadea5348
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2026-05-13 14:29:04 +0200

    fix: scriptlet charm config get

--- domain/schema/model/sql/0058-scriptlet-charm.PATCH.sql
+++ domain/schema/model/sql/0058-scriptlet-charm.PATCH.sql
@@ -4,3 +4,18 @@ CREATE TABLE scriptlet_charm (
     CONSTRAINT fk_scriptlet_charm_charm
     FOREIGN KEY (charm_uuid) REFERENCES charm (uuid)
 );
+
+DROP VIEW IF EXISTS v_application_origin;
+
+CREATE VIEW v_application_origin AS
+SELECT
+    a.uuid,
+    c.reference_name,
+    c.source_id,
+    c.revision,
+    cdi.charmhub_identifier,
+    ch.hash
+FROM application AS a
+JOIN charm AS c ON a.charm_uuid = c.uuid
+LEFT JOIN charm_download_info AS cdi ON c.uuid = cdi.charm_uuid
+LEFT JOIN charm_hash AS ch ON c.uuid = ch.charm_uuid;
